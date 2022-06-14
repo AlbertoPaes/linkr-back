0930771498c1759e676375d93dfcb0d9844c2afa
Alberto <tejotaesi@gmail.com>
2022-06-14 11:40:53 -0300

    first version

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {Pool} = pg;
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if(process.env.MODE === 'PROD'){
+  databaseConfig.ssl = {
+    rejectUnauthorized: false
+  }
+}
+
+const db = new Pool(databaseConfig);
+
+export default db;
---
+++ index.js
@@ -0,0 +1,19 @@
+import express, {json} from 'express';
+import chalk from 'chalk';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import router from './routers/index.js';
+
+const server = express();
+
+server.use(json());
+server.use(cors());
+
+server.use(router);
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(chalk.bold.green(`Listening on ${PORT}`));
+});
