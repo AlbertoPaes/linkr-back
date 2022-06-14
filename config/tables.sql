c6c09927e0ecf21e0a03bf4afa19828733e90674
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-06-14 09:45:37 -0300

    first configs

---
+++ config/tables.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  email text NOT NULL UNIQUE,
+  password text NOT NULL,
+  image text NOT NULL,
+  "createdAt" timestamp DEFAULT NOW()
+);
+
+CREATE TABLE sessions (
+  id serial PRIMARY KEY,
+  "userId" integer NOT NULL REFERENCES "users"("id"),
+  token text NOT NULL,
+  "createdAt" timestamp DEFAULT NOW()
+);
+
+CREATE TABLE posts (
+  id serial PRIMARY KEY,
+  "userId" integer NOT NULL REFERENCES "users"("id"),
+  link text NOT NULL,
+  description text,
+  "createdAt" timestamp DEFAULT NOW()
+);
+
+CREATE TABLE "postLike" (
+  id serial PRIMARY KEY,
+  "userId" integer NOT NULL REFERENCES "users"("id"),
+  "postId" integer NOT NULL REFERENCES "posts"("id")
+);
+
+CREATE TABLE hashtags (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  "createdAt" timestamp DEFAULT NOW()
+);
+
+CREATE TABLE "postHashtag" (
+  id serial PRIMARY KEY,
+  "postId" integer NOT NULL REFERENCES "posts"("id"),
+  "hashtagId" integer NOT NULL REFERENCES "hashtags"("id")
+);
