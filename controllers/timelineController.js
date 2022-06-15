32c13fdbee928cd203c6daacb52acbb2503dc6cc
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-06-15 10:18:22 -0300

    fix: timeline foldes

--- controllers/timelineController.js
+++ controllers/timelineController.js
@@ -3,12 +3,25 @@ import chalk from "chalk"
 import { timelineRepository } from "../repositories/timelineRepository.js";
 
 export async function publishPost(req, res) {
-  const { user: { id } } = res.locals;
+  const { user: { id: userId } } = res.locals;
   const { link, description } = req.body;
-  //TODO: Pegar "userId" do midlleware de Token
+
+  //TODO: TENHO Q SALVAR TODAS AS HASHTAGS DA DESCRIPTION E NAO SOMENTE UMA
+  const [hashtag] = /#\w+/g.exec(description);
+  const hashtagLower = hashtag.toLowerCase();
 
   try {
-    await timelineRepository.insertPost(id, link, description);
+    await timelineRepository.insertPost(userId, link, description);
+
+    const { rows: posts } = await timelineRepository.searchOnePost(userId);
+    const [{ id: postId }] = posts;
+
+    const { rows: hashtags } = await timelineRepository.searchHashtags(hashtagLower);
+    const [hashtag] = hashtags;
+    if (!hashtag) await timelineRepository.insertHashtag(hashtagLower);
+
+    await timelineRepository.relatePostHashtag(postId,)
+
 
     res.sendStatus(201);
   } catch (e) {
