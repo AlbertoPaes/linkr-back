dd17894ce3f23d19595fa34f0a54ded87f8e0678
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-06-22 16:58:13 -0300

    fix: user page

--- routers/followsRouter.js
+++ routers/followsRouter.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+
 import { getFollows, postFollow, deleteFollow, searchFollows } from "../controllers/followsController.js";
+import validateToken from "../middlewares/tokenMiddleware.js";
 
 const followsRouter = Router();
 
 // COLOCAR OS MIDDLEWARES DE VALIDAÇÃO
 
-followsRouter.get('/follows/:userId/:followId', getFollows);
-followsRouter.get('/follows/search/:name/:userId', searchFollows);
-followsRouter.post('/follows', postFollow);
-followsRouter.delete('/follows/:userId/:followId', deleteFollow);
+followsRouter.get('/follows/:userId/:followId', validateToken, getFollows);
+followsRouter.get('/follows/search/:name/:userId', validateToken, searchFollows);
+followsRouter.post('/follows', validateToken, postFollow);
+followsRouter.delete('/follows/:userId/:followId', validateToken, deleteFollow);
 
 export default followsRouter;
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getUserPosts, searchUsers } from "../controllers/usersController.js";
+
+import { getUserName, getUserPosts, searchUsers } from "../controllers/usersController.js";
+import validateToken from "../middlewares/tokenMiddleware.js";
 
 const usersRouter = Router();
 
-usersRouter.get("/users/:id", getUserPosts);
-usersRouter.get("/search/:name", searchUsers);
+usersRouter.get("/users/:id", validateToken, getUserPosts);
+usersRouter.get("/profile/:id", validateToken, getUserName);
+usersRouter.get("/search/:name", validateToken, searchUsers);
 
 export default usersRouter;
